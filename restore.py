39bce9a752c6be7cb7e6924c5bd5027fdcd5ee31
HARISH L <48373859+Harish7nov@users.noreply.github.com>
2020-08-16 23:40:59 +0530

    Update restore.py

--- restore.py
+++ restore.py
@@ -3,22 +3,27 @@ import time
 import pyperclip
 
 # Giving time for the user to open a new chrome tab
-time.sleep(3)
-path = r"D:\Coding files\PYTHON\Chrome Automation\links.txt"
+time.sleep(5)
+path = r"<path to the txt file to read all the saved links>"
 
+# Open the file
 f = open(path,"r+")
 
+# Read all the links into a list
 links = [i.strip() for i in f.readlines()]
 
 f.close()
 
+# Start opening the links
 for i in links:
 	pyautogui.click(319, 74)
 
 	# Enter the links in the chrome browser tab
 	pyperclip.copy(i)
+	
 	# Paste the copied link and access the site
 	pyautogui.hotkey("ctrl", "v")
+	
 	pyautogui.hotkey("enter")
 
 	# Open the next tab
